2cebbc2a1510cd8dee80cb67434876f091e11abd
kongou1913 <haluka0130@gmail.com>
2019-02-21 12:36:46 +0000

    2ndcommit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,13 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.new(name: params[:user][:name], email: params[:user][:email])
     if @user.save
-      redirect_to topics_path, success: '登録が完了しました'
+      redirect_to root_path
     else
-      flash.now[:danger] = "登録に失敗しました"
       render :new
     end
-  end
-
-  private
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
