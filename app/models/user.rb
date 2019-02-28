c2d67c03e9c7ab648a3d633d16ab8d48d3b8b0ac
kongou1913 <haluka0130@gmail.com>
2019-02-28 12:44:58 +0000

    kadaiteisyutu

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ApplicationRecord
+  has_secure_password
   validates :name, presence: true, length: {maximum: 15}
   ML_REGEX = /[A-Za-z0-9._+]+@[A-Za-z]+.[A-Za-z]/
   validates :email, presence: true, format: { with: ML_REGEX }
   PW_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/i
   validates :password, presence: true, format: { with: PW_REGEX }
-
-  has_secure_password
 
   has_many :topics
   has_many :favorites
