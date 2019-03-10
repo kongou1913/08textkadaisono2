ce319f69b07efb27cf711f3d56b9df999a3897b5
kongou1913 <haluka0130@gmail.com>
2019-03-10 11:11:35 +0000

    07kadai

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,6 +5,14 @@ class ImageUploader < CarrierWave::Uploader::Base
   #require_relative 'config/initializers/carrierwave'
   #require_relative 'config/initializers/database'
 
+  def size_range
+    1..10.megabytes
+  end
+
+  def extension_white_list
+    %w(jpg jpeg png)
+  end
+  
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
