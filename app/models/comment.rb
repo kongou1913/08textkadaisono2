a72dffc99843664710e4077660fc047e69138cce
kongou1913 <haluka0130@gmail.com>
2019-03-27 13:55:27 +0000

    tochuu

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  validates :t_content, presence: true, length: {maximum: 1000 }
+  validates :lines, presence: true, length: {maximum: 1000 }
 
   belongs_to :user
   belongs_to :topic
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,11 @@ Rails.application.routes.draw do
 
   resources :topics
 
-get 'favorites/index'
-post '/favorites', to: 'favorites#create'
+  get 'favorites/index'
+  post '/favorites', to: 'favorites#create'
+  delete '/favorites', to: 'favorites#destroy'
+
+  get 'comments', to: 'comments#new'
+  post '/comments', to: 'comments#create'
 
 end
